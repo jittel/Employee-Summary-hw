b8078badfc506c2de851d5c093b9f436cdfb04d8
jittel <nathan.c.ginn@gmail.com>
2020-01-07 20:37:48 -0800

    first commit

---
+++ Develop/index.js
@@ -0,0 +1,103 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+
+const appendFileAsync = util.promisify(fs.writeFile);
+
+async function promptUser() {
+    try {
+        let employee = await inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is your name?"
+            },
+            {
+                type: "input",
+                name: "role",
+                message: "What is your role?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your ID number?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your email?"
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is your github?"
+            }
+        ]);
+        console.log(employee)
+
+        let name = JSON.stringify(employee.name);
+        let role = JSON.stringify(employee.role);
+        let id = JSON.stringify(employee.id);
+        let email = JSON.stringify(employee.email);
+        let github = JSON.stringify(employee.github);
+        let path = employee.role
+
+        //MAKE IT SO THAT IT WRITES A DIFFERENT HTML CARD DEPENDING ON ANSWER TO ROLE (instead of files have the html templates as variables)
+        switch (path) {
+            case "intern":
+                //stuff here
+                const intern = `<div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title">${name}</h5>
+  <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+  <p class="card-text">ID: ${id}</p>
+  <a href="#" class="card-link">${email}</a>
+  <a href=${github} class="card-link">github</a>
+</div>
+</div>`
+
+                const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+    <div>${intern}</div>
+</body>
+</html>`
+                appendFileAsync("index.html", html, "utf8")
+                console.log(employee.role)
+
+                let addAnother = await inquirer.prompt({
+                    type: "input",
+                    name: "continue",
+                    message: "would you like to add another employee?"
+                });
+                console.log(addAnother.continue)
+
+                if (addAnother.continue === "yes") {
+                    promptUser();
+                } else {
+                    break;
+                }
+            case "engineer":
+                //stuff here
+                console.log(employee.role)
+                break;
+            case "manager":
+                //stuff here
+                console.log(employee.role)
+                break;
+            default:
+                console.log("wrong role")
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+promptUser();
